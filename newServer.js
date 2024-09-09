a6371c9a57cabc1b1548b2482c08af7f1778c057
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-09 16:32:25 +0200

    Update newServer.js

--- newServer.js
+++ newServer.js
@@ -14,7 +14,7 @@ let proxies = loadProxies();
 let botsRunning = false;
 
 
-static async sendRequest(agent) {
+ async function sendRequest(agent) {
   try {
     // Launch Puppeteer browser instance with proxy settings
     const browser = await puppeteer.launch({
