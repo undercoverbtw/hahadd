18672e4b0db339fcad6aedbcfb754e4258052d56
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-19 10:30:32 +0200

    Update test.js

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ async function testProxy(proxy) {
 async function main() {
     const proxy = {
         host: 'proxy.toolip.io',
-        port: '_port_',
+        port: '8087',
         auth: {username: '8c5906b99fbd1c0bcd0f916d545c565ae81b604736435265b767d885a09cd1c5494da4fd86d192b8bf7df0dd1415f735-country-XX-session-###', password: 'wn1k0xg25fxa'},
     };
     await testProxy(proxy);
